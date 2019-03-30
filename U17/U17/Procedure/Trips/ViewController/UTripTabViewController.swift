35faf7035719be393db0ee9c71bf40b92cfbffe4
HaibaraAi96 <369579965@qq.com>
2019-03-30 14:41:37 -0700

    added new assets

---
+++ U17/U17/Procedure/Trips/ViewController/UTripTabViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UTripTabViewController.swift
+//  U17
+//
+//  Created by hanjing on 3/30/19.
+//  Copyright © 2019 None. All rights reserved.
+//
+
+import UIKit
+
+class UTripTabViewController: UBaseViewController {
+    
+    // MARK: Properties
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
